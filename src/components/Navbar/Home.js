e12a4b046cad571c8b3d3e92aec093ef979ae165
MaheshBhadane <mahesh.bhadane@torinit.ca>
2022-08-16 18:48:53 +0530

    Updated button on home

--- src/components/Navbar/Home.js
+++ src/components/Navbar/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from './Hero1.png';
 import './Home.css';
+import { Link } from "react-router-dom"
 
 const Home = () => {
  
@@ -9,10 +10,12 @@ const Home = () => {
     <div class="content">
     <center>
       <h2>HOME</h2>
+      <Link to="/Admin">
       <img src={Hero} alt="" width="1500" height="500"/>
-      <button>Add New Item Here..</button>
+      <button type="button">Add New Item Here..</button>
+      </Link>
       </center>
-    </div>
+      </div>
 
     </>
   );
